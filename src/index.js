fca98209614f5423269b5e3dd9d755452df7bb49
malekylik <malekylik@gmail.com>
2018-03-11 21:48:53 +0300

    impliment some function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,109 @@
 
 const createEnumerableProperty = () => {};
 const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+
+const createProtoMagicObject = () => {
+
+    let f = function(){};
+
+    f.prototype = f.__proto__;
+
+    return f;
+};
+const incrementor = () => {
+    if(!incrementor.count){
+        incrementor.count = 0;
+
+        incrementor.toString = function(){
+            return  incrementor.count;
+        }
+    }
+
+    incrementor.count++;
+
+    return incrementor;
+};
+
+
+
 const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+
+
+const createIncrementer = () => {
+    
+    let value = 0;
+    let inc = {  
+        next(){
+            value++;
+    
+            return {
+                done: false,
+                value:value
+              };
+        }
+    };
+
+    inc[Symbol.iterator] = () => {
+        return {next:inc.next};
+    };
+
+        return inc;
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
+const returnBackInSecond = (param) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+        resolve(param);
+      }, 1000);
+    });
+};
+
+const getDeepPropertiesCount = (obj) => {
+    let count = 0;
+
+    for(let key in obj){
+        count++;
+        count += getDeepPropertiesCount(obj[key]);
+    }
+
+    return count;
+};
+
+const createSerializedObject = () => {
+   return null;
+};
+
+
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (array) => {
+    const temp = [];
+
+    for(let i = 0; i < array.length; i++){
+        temp.push({
+            index:i,
+            value:0
+        });
+        let proto = Object.getPrototypeOf(array[i]);
+        while(proto){
+            temp[i].value++;
+            proto = Object.getPrototypeOf(proto);
+        }
+    }
+
+    temp.sort((l,r) => {
+        return r.value - l.value;
+    });
+
+    const result = [];
+
+    for(let i = 0; i < array.length; i++){
+        result.push(array[temp[i].index]);
+    }
+
+    return result;
+
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
@@ -24,3 +116,5 @@ exports.returnBackInSecond = returnBackInSecond;
 exports.getDeepPropertiesCount = getDeepPropertiesCount;
 exports.createSerializedObject = createSerializedObject;
 exports.sortByProto = sortByProto;
+
+
